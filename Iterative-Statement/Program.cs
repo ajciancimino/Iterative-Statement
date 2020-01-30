395c2c830ad2bfd2aaaadd4b2afe426a9fea8b22
Ayrton Ciancimino <ajciancimino@usf.edu>
2020-01-30 18:06:09 -0500

    Final Commit

--- Iterative-Statement/Program.cs
+++ Iterative-Statement/Program.cs
@@ -15,16 +15,22 @@ namespace Iterative_Statement
                 // This variable will store the input and parse it as an input
                 int inputValue = int.Parse(input);
 
+                if ((inputValue > 0) && (inputValue <= 100))
 
-
-                // This is the for-loop; It will count to the inputed number
-                for (int i = 0; i <= inputValue; i++)
                 {
-                    Console.WriteLine("You have entered " + inputValue.ToString() + ". This is the current integer value in the loop:" + i.ToString());
+
+                    // This is the for-loop; It will count to the inputed number
+                    for (int i = 1; i <= inputValue; i++)
+                    {
+                        Console.WriteLine("You have entered " + inputValue.ToString() + ". This is the current integer value in the loop:" + i.ToString());
+                    }
+                    Console.WriteLine("Press any key to exit the program ...");
+                    // Pause the program and await the user to press a key to end the program
+                    Console.ReadKey(true);
+
                 }
-                Console.WriteLine("Press any key to exit the program ...");
-                // Pause the program and await the user to press a key to end the program
-                Console.ReadKey(true);
+                else
+                    Console.WriteLine("Input must be an integer between 0 and 100");
             }
             catch
             {
